e8c16ee7602d75675c537bc2b8f98b040841147b
bartek <batorskib@gmail.com>
2023-01-08 17:28:16 +0100

    #point

---
+++ src/Arrays/Tablice.java
@@ -0,0 +1,15 @@
+package src.Arrays;
+
+public class Tablice {
+    public static void main(String[] args) {
+
+
+        int[] ints = {1,3,5};
+        int[] ints2 = {2,4,6};
+
+        ArraysUtlis arraysUtlis = new ArraysUtlis();
+
+        System.out.println(arraysUtlis.sumArrays(ints,ints2));
+
+    }
+}
