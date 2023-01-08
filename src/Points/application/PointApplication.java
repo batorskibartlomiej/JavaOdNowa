b85598991e90dcfa6401be0eec04aedfe62fefc7
bartek <batorskib@gmail.com>
2023-01-08 17:28:16 +0100

    #point

--- src/Points/application/PointApplication.java
+++ src/Points/application/PointApplication.java
@@ -6,18 +6,52 @@ import src.Points.data.Point;
 public class PointApplication {
 
     public static void main(String[] args) {
-        Point a = new Point(5, 6);
+
+
+
+
+
+
+        Point p1 = new Point(5, 6);
         PointController pc = new PointController();
 
-        System.out.println("Punkt: (" + a.getX()+";"+ a.getY()+")");
-        pc.addX(a);
-        System.out.println("Punkt addX: (" + a.getX()+";"+ a.getY()+")");
-        pc.addY(a);
-        System.out.println("Punkt addY: (" + a.getX()+";"+ a.getY()+")");
-        pc.minusX(a);
-        System.out.println("Punkt minusX: (" + a.getX()+";"+ a.getY()+")");
-        pc.minusY(a);
-        System.out.println("Punkt minusY: (" + a.getX()+";"+ a.getY()+")");
+
+        final int addX = 0, addY = 1;
+        final int minusX = 3;
+        final int minusY = 4;
+
+
+        int option = minusX;
+        switch (option) {
+            case addX:
+                pc.addX(p1);
+                break;
+            case addY:
+                pc.addY(p1);
+                break;
+            case minusX:
+                pc.minusX(p1);
+                break;
+            case minusY:
+                pc.minusY(p1);
+                break;
+            default:
+                System.out.println("Podano błędną wartość");
+        }
+
+        System.out.println("Punkt po zmianie: (" + p1.getX() + ";" + p1.getY() + ")");
+
+//        System.out.println("Punkt: (" + a.getX()+";"+ a.getY()+")");
+//        pc.addX(a);
+//        System.out.println("Punkt addX: (" + a.getX()+";"+ a.getY()+")");
+//        pc.addY(a);
+//        System.out.println("Punkt addY: (" + a.getX()+";"+ a.getY()+")");
+//        pc.minusX(a);
+//        System.out.println("Punkt minusX: (" + a.getX()+";"+ a.getY()+")");
+//        pc.minusY(a);
+//        System.out.println("Punkt minusY: (" + a.getX()+";"+ a.getY()+")");
+
+
 
     }
 
