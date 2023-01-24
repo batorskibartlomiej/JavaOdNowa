fb093eb5e2f7a1a589f79d53c5206beb802e6345
bartekbatorski <batorskib@gmail.com>
2023-01-24 20:02:50 +0100

    #test

---
+++ src/SumNumbers/SumNumbers.java
@@ -0,0 +1,33 @@
+package src.SumNumbers;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ile liczba chcesz podać");
+        int liczba = scanner.nextInt();
+
+        int sum = 0;
+
+        int i=0;
+
+
+
+
+        while ( liczba >0){
+
+            System.out.println("podaj kolejną liczbę");
+            sum = sum + scanner.nextInt();
+            liczba--;
+        }
+
+        System.out.println("suma wszystkich liczb "+ sum);
+        scanner.close();
+
+
+
+    }
+}
