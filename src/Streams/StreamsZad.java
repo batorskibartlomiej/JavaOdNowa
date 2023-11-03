7587767d959a4320a5790f738c4398f92a744070
Bartek <batorskib@gmail.com>
2023-11-03 16:07:08 +0100

    #stream

---
+++ src/Streams/StreamsZad.java
@@ -0,0 +1,16 @@
+package src.Streams;
+
+import java.util.stream.Stream;
+
+public class StreamsZad {
+
+    public static void main(String[] args) {
+         Stream.iterate(0,x -> x + 1)
+                 .filter(x->x>100&& x<1000&& x%5==0)
+                 .limit(10)
+                 .map(x->x*3)
+                 .forEach(System.out::println);
+
+
+    }
+}
